ad418054aad5ce166dd6f7c9783e8ab94f04dcf9
PRamoneda <pppramoneda@hotmail.com>
2021-05-21 14:24:50 +0200

    refactor pianoplayer main

--- pianoplayer/__init__.py
+++ pianoplayer/__init__.py
@@ -1,3 +1,9 @@
+import os, sys
+from music21 import converter, stream
+from pianoplayer.hand import Hand
+from pianoplayer.scorereader import reader, PIG2Stream, reader_pretty_midi
+from operator import attrgetter
+import pretty_midi
 ###########################################################
 __author__ = "Marco Musy"
 __email__ = "marco.musy@gmail.com"
@@ -5,8 +11,6 @@ __version__ = "2.2.0"  # defined in setup.py
 __status__ = "dev"
 __website__ = "https://github.com/marcomusy/pianoplayer"
 __license__ = "MIT"
-
-
 ###########################################################
 # Piano Player main analyse and annotate
 ###########################################################
