d87d78f8e4efef1c7996988066b94233a16e8aca
PRamoneda <pppramoneda@hotmail.com>
2021-05-24 11:40:24 +0200

    add

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ setup(
     version='2.2.0', #change also in __init__.py
     packages=['pianoplayer'],
     scripts=['bin/pianoplayer'],
-    install_requires=['music21','vedo'],
-    description='Automatic piano fingering generator. Finds and shows in 3D the best fingering combination to play a score.',
+    install_requires=['music21', 'vedo', 'pretty_midi'],
+    description=
+    'Automatic piano fingering generator. Finds and shows in 3D the best fingering combination to play a score.',
     long_description="""Automatic piano fingering generator.
     Finds and shows in a 3D visualization the best fingering combination to play a score.
 
@@ -17,11 +18,11 @@ setup(
     url='https://github.com/marcomusy/pianoplayer',
     keywords='piano fingering',
     classifiers=['Intended Audience :: Science/Research',
-                'Intended Audience :: Education',
-                'Programming Language :: Python',
-                'License :: OSI Approved :: MIT License',
-                'Topic :: Scientific/Engineering :: Visualization',
-                'Topic :: Scientific/Engineering :: Information Analysis'],
+                 'Intended Audience :: Education',
+                 'Programming Language :: Python',
+                 'License :: OSI Approved :: MIT License',
+                 'Topic :: Scientific/Engineering :: Visualization',
+                 'Topic :: Scientific/Engineering :: Information Analysis'],
     include_package_data=True
 )
 
