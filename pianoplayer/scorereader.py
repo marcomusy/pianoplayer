8886cbc812e5109918639d985e1d4158c804ce9e
marcomusy <marco.musy@gmail.com>
2026-02-25 02:00:37 +0100

    refactor(scorereader): apply audit cleanups

--- pianoplayer/scorereader.py
+++ pianoplayer/scorereader.py
@@ -63,9 +63,14 @@ def reader(sf: Any, beam: int = 0) -> list[INote]:
             an.duration = n.duration.quarterLength
             pc = n.pitch.pitchClass
             an.isBlack = pc in [1, 3, 6, 8, 10]
-            if n.lyrics:
+
+            # Prefer explicit articulation fingering; fallback to lyric fingering.
+            art_finger = get_finger_music21(n)
+            if art_finger:
+                an.fingering = art_finger
+            elif n.lyrics:
                 an.fingering = n.lyric
-            an.fingering = get_finger_music21(n)
+
             noteseq.append(an)
             note_id += 1
             continue
@@ -106,64 +111,70 @@ def reader(sf: Any, beam: int = 0) -> list[INote]:
 def reader_pretty_midi(pm: Any, beam: int = 0) -> list[INote]:
     noteseq: list[INote] = []
     pm_notes = sorted(pm.notes, key=attrgetter("start"))
-    pm_onsets = [onset.start for onset in pm_notes]
 
     logger.info("Reading beam %s with %s objects in stream.", beam, len(pm_notes))
+
     chord_id = 0
+    note_id = 0
+    sfasam = 0.05
 
     ii = 0
     while ii < len(pm_notes):
-        n = pm_notes[ii]
-        n_duration = n.end - n.start
-        chord_notes = pm_onsets.count(n.start)
+        onset = pm_notes[ii].start
+        jj = ii + 1
+        while jj < len(pm_notes) and pm_notes[jj].start == onset:
+            jj += 1
 
-        if chord_notes != 1:
-            if n_duration == 0:
-                ii += 1
-                continue
+        group = pm_notes[ii:jj]
+        group_size = len(group)
 
-            an = INote()
-            an.noteID += 1
-            an.note21 = n
-            an.pitch = n.pitch
-            an.isChord = False
-            an.octave = n.pitch // 12
-            an.x = keypos_midi(n)
-            an.time = n.start
-            an.duration = n_duration
-            pc = n.pitch % 12
-            an.isBlack = pc in [1, 3, 6, 8, 10]
-            noteseq.append(an)
-            ii += 1
+        if group_size == 1:
+            n = group[0]
+            n_duration = n.end - n.start
+            if n_duration > 0:
+                an = INote()
+                an.noteID = note_id
+                note_id += 1
+                an.note21 = n
+                an.pitch = n.pitch
+                an.isChord = False
+                an.octave = n.pitch // 12
+                an.x = keypos_midi(n)
+                an.time = n.start
+                an.duration = n_duration
+                pc = n.pitch % 12
+                an.isBlack = pc in [1, 3, 6, 8, 10]
+                noteseq.append(an)
+            ii = jj
             continue
 
-        if n_duration == 0:
-            ii += 1
+        valid_group = [n for n in group if (n.end - n.start) > 0]
+        if not valid_group:
+            ii = jj
             continue
 
-        sfasam = 0.05
-        for jj in range(chord_notes):
-            cn = pm_notes[ii]
+        for k, cn in enumerate(valid_group):
             cn_duration = cn.end - cn.start
             an = INote()
             an.chordID = chord_id
-            an.noteID += 1
+            an.noteID = note_id
+            note_id += 1
             an.isChord = True
-            an.chord21 = n
+            an.chord21 = group[0]
             an.pitch = cn.pitch
             an.note21 = cn
-            an.chordnr = jj
-            an.NinChord = chord_notes
-            an.octave = cn.pitch // 2
+            an.chordnr = k
+            an.NinChord = len(valid_group)
+            an.octave = cn.pitch // 12
             an.x = keypos_midi(cn)
-            an.time = cn.start - sfasam * jj
-            an.duration = cn_duration + sfasam * (jj - 1)
-            pc = n.pitch % 12
+            an.time = onset - sfasam * (len(valid_group) - k - 1)
+            an.duration = cn_duration + sfasam * (len(valid_group) - 1)
+            pc = cn.pitch % 12
             an.isBlack = pc in [1, 3, 6, 8, 10]
             noteseq.append(an)
-            ii += 1
 
         chord_id += 1
+        ii = jj
 
     if len(noteseq) < 2:
         logger.info("Beam is empty.")
@@ -179,8 +190,8 @@ def reader_PIG(fname: str, beam: int = 0) -> list[INote]:
     beam = 0, right hand
     beam = 1, left hand.
     """
-    del fname, beam
-    return []
+    sf = PIG2Stream(fname, beam=beam)
+    return reader(sf, beam=0)
 
 
 def PIG2Stream(fname: str, beam: int = 0, time_unit: float = 0.5, fixtempo: float = 0) -> Any:
@@ -214,6 +225,12 @@ def PIG2Stream(fname: str, beam: int = 0, time_unit: float = 0.5, fixtempo: floa
         durations.append(offset - onset)
         blines.append(line)
 
+    sf = stream.Part()
+    sf.id = beam
+
+    if not blines:
+        return sf
+
     durations_arr = np.array(durations)
     logdurs = -np.log2(durations_arr)
     mindur = np.min(logdurs)
@@ -221,33 +238,34 @@ def PIG2Stream(fname: str, beam: int = 0, time_unit: float = 0.5, fixtempo: floa
     if np.max(expos) > 3:
         mindur = mindur + 1
 
-    sf = stream.Part()
-    sf.id = beam
-
     if not fixtempo and firstonset:
         r = note.Rest()
         logdur = -np.log2(firstonset)
         r.duration.quarterLength = 1.0 / time_unit / pow(2, int(logdur - mindur))
         sf.append(r)
 
+    i = 0
     n_lines = len(blines)
-    for i in range(n_lines):
-        if blines[i].startswith("//"):
+    while i < n_lines:
+        line = blines[i]
+        if line.startswith("//"):
+            i += 1
             continue
 
-        _, onset, offset, name, _, _, _, finger = blines[i].split()
+        _, onset, offset, name, _, _, _, finger = line.split()
         onset, offset = float(onset), float(offset)
         name = name.replace("b", "-")
 
         chordnotes = [name]
-        for j in range(1, 5):
-            if i + j >= n_lines:
-                continue
-            _, onset1, _, name1, _, _, _, _ = blines[i + j].split()
+        next_i = i + 1
+        while next_i < n_lines:
+            _, onset1, _, name1, _, _, _, _ = blines[next_i].split()
             onset1 = float(onset1)
-            if onset1 == onset:
-                name1 = name1.replace("b", "-")
-                chordnotes.append(name1)
+            if onset1 != onset:
+                break
+            name1 = name1.replace("b", "-")
+            chordnotes.append(name1)
+            next_i += 1
 
         if len(chordnotes) > 1:
             an = chord.Chord(chordnotes)
@@ -266,17 +284,19 @@ def PIG2Stream(fname: str, beam: int = 0, time_unit: float = 0.5, fixtempo: floa
 
         sf.append(an)
 
-        if i + 1 < n_lines:
-            _, onset1, _, _, _, _, _, _ = blines[i + 1].split()
-            onset1 = float(onset1)
-            if onset1 - offset > 0:
+        if next_i < n_lines:
+            _, onset_next, _, _, _, _, _, _ = blines[next_i].split()
+            onset_next = float(onset_next)
+            if onset_next - offset > 0:
                 r = note.Rest()
                 if fixtempo:
                     r.duration.quarterLength = fixtempo
-                logdur = -np.log2(onset1 - offset)
+                logdur = -np.log2(onset_next - offset)
                 d = int(logdur - mindur)
                 if d < 4:
                     r.duration.quarterLength = 1.0 / time_unit / pow(2, d)
                     sf.append(r)
+
+        i = next_i
 
     return sf
